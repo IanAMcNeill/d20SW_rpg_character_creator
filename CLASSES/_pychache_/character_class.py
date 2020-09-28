3b217ce2e430d6f2d4e36362005368107f03f01e
ianmcneill7 <64739630+ianmcneill7@users.noreply.github.com>
2020-09-28 16:12:46 -0400

    Add files via upload

---
+++ CLASSES/_pychache_/character_class.py
@@ -0,0 +1,17 @@
+# a list of possible classes for player characters in SWRPG	
+starting_classes = [ 
+	'Fringer' , 'Noble' , 'Soldier' , 'Tech Specialist' , 'Force Adept' ,
+	'Jedi Guardian' , 'Scout' , 'Scoundrel'
+	]
+prestige_classes = [ 
+	'Bounty Hunter' , 'Crime Lord' , 'Dark Side Devotee' , 'Dark Side Marauder' , 
+	'Elite Trooper' , 'Jedi Ace' , 'Jedi Investigator' , 'Jedi Master' , 
+	'Officer' , 'Starship Ace' 
+	]
+
+droid_classifications = [
+	'First-Degree Droids' , 'Second-Degree Droids' , 'Third-Degree Droids' , 
+	'Fourth-Degree Droids' , 'Fifth-Degree Droids'
+	]
+
+
