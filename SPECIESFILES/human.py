d5fe53395231ec78b0580c0fb930317d74c2c70d
ianmcneill7 <64739630+ianmcneill7@users.noreply.github.com>
2020-09-28 20:05:40 -0400

    Create human.py

---
+++ SPECIESFILES/human.py
@@ -0,0 +1,28 @@
+# Human descriptions, traits and bonuses
+
+description = "DESCRIPTION : \n \"Humans dominate the core worlds and can be found in virtually every corner of the galaxy. Humans have always been people on the move. As a result they can be found on almost every inhabited planet. Human's remain one of the most dominant species throughout all eras of play.\" "
+
+personality = "PERSONALITY : \n \"Highly adaptable, tenacious, and willing to keep striving no matter the odds. They are flexble and ambitious, diverse in their tastes, morals, customs, and habits.\" "
+
+physicality = "PHYSICAL DESCRIPTION : \n \"Average height (1.8 meters tall). Skin shades range from dark to pale, hair from black to blonde. Men are usually taller and heavier than women. Human's achieve maturity about the age of 15 and rarely live beyond 100.\""
+
+homeworld = "HOMEWORLD : \n \"Various, including Coruscant, Corulag, Naboo, Tatooine and Alderaan.\""
+
+language = "LANGUAGE(S) : \n \"Humans speak and read Basic. They typically learn other languages, including obscure ones.\""
+
+example_names = "EXAMPLE NAMES : \n \" Anakin, Arani, Bail, Ben, Beru, Biggs, Boba, Corran, Dack, Galak, Han, Jango, Lando, Leia, Luke, Mace, Mara, Obi-Wan, Owen, Qui-Gon, Wedge.\"" 
+
+adventuring = "WHY THEY VENTURE OUT: \n \"Humans champion causes rather than territories or groups.\""
+
+trait_size = "Medium"
+trait_size.lower()
+size_bonus_defense = 0
+size_bonus_attack =  0
+size_bonus_hide =  0
+
+base_speed_meters = 10
+
+skill_pts = 4
+feats =  1
+
+auto_language = "Read/Write Basic , Speak Basic. "
