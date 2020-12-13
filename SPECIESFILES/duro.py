aafd532875872e0c0acb4b598162a70d484bc4cf
ianmcneill7 <64739630+ianmcneill7@users.noreply.github.com>
2020-12-13 12:36:22 -0500

    Add files via upload

---
+++ SPECIESFILES/duro.py
@@ -0,0 +1,38 @@
+# Duro descriptions, traits and bonuses
+
+description = "DESCRIPTION : \n \"Duros originate from the Duro System, located at the extreme edge of the Core region of space. With a near affinity to space travel, they were the first to leave their world and spread to the stars. Duros tend to stick to space lanes for income and adventure. Most Duro become trained pilots after learning to walk and talk.\" "
+
+personality = "PERSONALITY : \n \"A Duros tends to be intesne and adventurous, always seeking to learn whats at the end of the next hyperspace jump. Pround, self-sufficient, fun-loving people who also have a tendency toward gregariousness.\" "
+
+physicality = "PHYSICAL DESCRIPTION : \n \"Average height of a Duros is 1.8m tall. Hairless, with large eyes, wide lipless mouths and a skin color ranging from blue-gray to deep azure.\""
+
+homeworld = "HOMEWORLD : \n \"They hail from the orbiting cities of the Duro System.\""
+
+language = "LANGUAGE(S) : \n \"Duros are literate in Durese, and Basic.\""
+
+example_names = "EXAMPLE NAMES : \n \" Baniss Keeg, Ellor, Kadlo, Kir Vantai, Lai Nootka, Monnda Tebbo.\"" 
+
+adventuring = "WHY THEY VENTURE OUT: \n \"Duros adventurers include hyperspace explorers, star charters, and spacers of all descriptions. They also gravitate toward the sciences. Some would rather smuggle and trade while few go into diplomacy.\""
+
+strAdj = 0
+dexAdj = 2
+intAdj = 2
+strAdj = -2
+conAdj = -2
+wisAdj = 0
+chaAdj = 0
+
+trait_size = "Medium"
+trait_size.lower()
+size_bonus_defense = 0
+size_bonus_attack =  0
+size_bonus_hide =  0
+
+base_speed_meters = 10
+
+skill_pts = 4
+feats =  1
+
+auto_language = "Read/Write: Durese, and Basic , Speak: Basic and Durese "
+
+
