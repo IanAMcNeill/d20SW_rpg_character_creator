3479bfcced6fb58dedf39ea8b7e8aaf874ded42b
ianmcneill7 <64739630+ianmcneill7@users.noreply.github.com>
2020-12-13 17:27:37 -0500

    Add files via upload

---
+++ SPECIESFILES/gungan.py
@@ -0,0 +1,46 @@
+# Cerean descriptions, traits and bonuses
+
+description = "DESCRIPTION : \n \"Gugnans are an ancient species from Naboo with a warlike tradition. Gungans can hold their breath for extended periods of time, and they live within underwater cities. The most common Gungan race is the Otolla Gungan, characterized by taller, leaner frames and extended eyestalks. The rare Ankura Gungan has recessed eyes, gren skin, and a heavier, shorter frame. They are usually distrustful of strangers and slow to accept aid, Gungans are nevertheless brave, strong, and confident of their own traditions.\" "
+
+personality = "PERSONALITY : \n \"Gungans are inquisive, cautious, and suspicious.\" "
+
+physicality = "PHYSICAL DESCRIPTION : \n \"They range from 1.6 to 2 meters tall, with two eyestalks, floppy ears, and long tongues.\""
+
+homeworld = "HOMEWORLD : \n \"Naboo (with colonies on the Ohma-D'un moon.\""
+
+language = "LANGUAGE(S) : \n \"Gungans speak gungan and Basic\""
+
+example_names = "EXAMPLE NAMES : \n \"Fassa, Jar Jar, Rugor, Tarpals, Toba, Tobler Ceel, Yoss.\"" 
+
+adventuring = "WHY THEY VENTURE OUT: \n \"Many leave their cities to explore the ancient swamps andshorelines. Since the treaty with the Humans of Naboo, some Gungans have taken a larger step into galacitc society. These brave individuals are driven by an impulse to explore and regain the warrior spirit of old. There have yet to be any Jedi from these people but there have been legends of adepts.\""
+
+adjustments = "Species Abilities: \n \"-2 WIS | +2 CON (Preconfigured) \""
+
+special1 = "\"Special Trait: Gungans can hold their breath for a number of rounds equal to 25 times his CON score. \n \
+            Special Trait: +4 species bonus on Swim checks.\n \
+            Special Trait: Low-Light vision: Can see twice as far as humans in dim light.\n \
+            Special Trait: +2 species bonus on Listen checks \n \
+            Special Trait: +1 species bonus on Reflex saves\"\n"
+
+
+strAdj = 0
+dexAdj = 0
+intAdj = 0
+wisAdj = -2
+conAdj = 2
+chaAdj = 0 
+
+trait_size = "Medium"
+trait_size.lower()
+size_bonus_defense = 0
+size_bonus_attack =  0
+size_bonus_hide =  0
+
+base_speed_meters = 10
+
+skill_pts = 4
+feats =  1
+
+auto_language = "Read/Write Basic , Speak Basic. "
+
+
