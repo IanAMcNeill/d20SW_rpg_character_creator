d7e0bcdc7739b50896a796bf15f4d9b426fc70b2
ianmcneill7 <64739630+ianmcneill7@users.noreply.github.com>
2020-12-13 21:40:32 -0500

    Add files via upload

---
+++ SPECIESFILES/ithorian.py
@@ -0,0 +1,54 @@
+# Ithorian descriptions, traits and bonuses
+
+description = "DESCRIPTION : \n \"Ithorians hail from the jungle world of Ithor or one of its many spaceworrthy \"herd\" \
+ships. Also known as Hammerheads by the galaxy at large, Ithorians are a peaceful people. Ithorians dedicate themselves \
+to ecological endeavors, especially on their homeworld, where they seek to preserve the pastoral beauty of the great \
+tropical jungle- the Mother Jungle. A gentle people, Ithorians have a great love of art. Many travel the galaxy \
+in giant hyperdrive heard ships designed to serve as ecological replicas of the Ithorian homeworld, \
+bringing the beauty of ithor wherever they go.\" "
+
+personality = "PERSONALITY : \n \"Tend to be calm, peaceful, tranquil, and gentle. \" "
+
+physicality = "PHYSICAL DESCRIPTION : \n \"Ithorians are humanoid, ranging in height from 1.8 to 2.3 meters tall, \
+with long necks that curl forward and end in dome-shaped heads. They have two mouths, one on each side of their\
+neck, producing a stereo effect when they talk.\""
+
+homeworld = "HOMEWORLD : \n \"Ithor, or a specific herd ship.\""
+
+language = "LANGUAGE(S) : \n \"They can speak stereophonic Ithorese and Basic.\""
+
+example_names = "EXAMPLE NAMES : \n \"fandomar, Momaw, Oraltor, Tomla, Trangle, Umwaw.\"" 
+
+adventuring = "WHY THEY VENTURE OUT: \n \"Ithorians tend to concentrate on peaceful professions. They love to meet \
+new beings and see new places. They enjoy negotiating deals and finding peaceful solutions to conflict. They have a\
+spiritual nature tied to their devotion to the Mother Jungle of their homeworld. Often, a wanderlust leads them to \
+explore the greater galaxy for a time before they eventually return to Ithor or the herd ship that spawned them.\""
+
+adjustments = "Species Abilities: \n \" +2 WIS | +2 CHA | -2 DEX \""
+
+special1 = "\"Special Trait: +2 Species bonus on Knwowledge (wilderness lore) checks. \n \
+Special Trait: +4 species bonus on Survival Checks \n \
+Special Trait: +1 species bonus on Will saves \" \n" 
+
+
+strAdj = 0
+dexAdj = -2
+intAdj = 0
+wisAdj = 2
+conAdj = 0
+chaAdj = 2
+
+trait_size = "Medium"
+trait_size.lower()
+size_bonus_defense = 0
+size_bonus_attack =  0
+size_bonus_hide =  0
+
+base_speed_meters = 10
+
+skill_pts = 4
+feats =  1
+
+auto_language = "Read/Write Basic , Speak Basic. "
+
+
