e6aca4e5bdb0c789f98110c2c009a550d1e2c491
ianmcneill7 <64739630+ianmcneill7@users.noreply.github.com>
2020-12-13 20:48:58 -0500

    Update bothan.py

--- SPECIESFILES/bothan.py
+++ SPECIESFILES/bothan.py
@@ -1,18 +1,26 @@
 # Human descriptions, traits and bonuses
+description = "DESCRIPTION : \n \" Bothans deal in information. They use it as a measure of wealth and power, \
+    even wielding it as a weapon when necessary. Culturally, Bothans do not believe in direct conflict, but\
+    rather prefer to manipulate situations from behind the scenes. They watch and wait for opportunities to \
+    spring up and use their information and cunning to gain status and influence. \" "
 
-description = "DESCRIPTION : \n \" Bothans deal in information. They use it as a measure of wealth and power, even wielding it as a weapon when necessary. Culturally, Bothans do not believe in direct conflict, but rather prefer to manipulate situations from behind the scenes. They watch and wait for opportunities to spring up and use their information and cunning to gain status and influence. \" "
+personality = "PERSONALITY : \n \"Manipulative, crafty, suspicious and curious by nature, and a little paranoid.\
+     They can be irritating, but are also loyal and brave.\" "
 
-personality = "PERSONALITY : \n \"Manipulative, crafty, suspicious and curious by nature, and a little paranoid. They can be irritating, but are also loyal and brave.\" "
+physicality = "PHYSICAL DESCRIPTION : \n \"Bothans are covered with fur that rippples in response to their \
+    emotions. They have tapered ears and both the males and females of the species sport beards. They are about \
+    1.6 meters tall with a maturity rate and lifespan slightly greater than Humans\""
 
-physicality = "PHYSICAL DESCRIPTION : \n \"Bothans are covered with fur that rippples in response to their emotions. They have tapered ears and both the males and females of the species sport beards. They are about 1.6 meters tall with a maturity rate and lifespan slightly greater than Humans\""
-
-homeworld = "HOMEWORLD : \n \"The Industrial world of Bothawui, with various colony worlds throughout the Mid Rim Territories, including Kothlis and Torolis.\""
+homeworld = "HOMEWORLD : \n \"The Industrial world of Bothawui, with various colony worlds throughout the Mid Rim \
+    Territories, including Kothlis and Torolis.\""
 
 language = "LANGUAGE(S) : \n \"Speak and Read Basic and Bothese.\""
 
 example_names = "EXAMPLE NAMES : \n \" Borsk Fey\'lya, Karka Kre\'fey, Koth Melan, Tav Breil\'lya, Tereb Ab'\lon.\"" 
 
-adventuring = "WHY THEY VENTURE OUT: \n \"Their curiosity leads some to be explorers and scouts. Even when a Bothan takes up a noble cause, his or her cultural upbringing makes it hard not to look for the profit and prestige in the situation.\""
+adventuring = "WHY THEY VENTURE OUT: \n \"Their curiosity leads some to be explorers and scouts. Even when a Bothan\
+     takes up a noble cause, his or her cultural upbringing makes it hard not to look for the profit and prestige in \
+     the situation.\""
 
 adjustments = "Species Abilities: \n \" +2 Dex | -2 Con (Preconfigured) \""
 
