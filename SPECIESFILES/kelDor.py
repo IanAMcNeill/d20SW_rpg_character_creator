6504cf36d3a0c6aa1d20d0ae3105e686b8b2567b
ianmcneill7 <64739630+ianmcneill7@users.noreply.github.com>
2020-12-13 21:52:34 -0500

    Add files via upload

---
+++ SPECIESFILES/kelDor.py
@@ -0,0 +1,49 @@
+# Kel Dor descriptions, traits and bonuses
+
+description = "DESCRIPTION : \n \"The Kel Dor originate on the planet Dorin, where they developed in an atmosphere of helium anda  gas unique \
+to their world. Consequently, more common atmospheres, including those that constist of oxygen, nitrogen, and carbon dioxide, are deadly to the\
+Kel Dor. Those Kel Dor that operate beyond the confines of their homeworld must wear breath masks and protective goggles. They have a long and pround Force\
+tradition though they have adapted it to the Jedi tradition since joining the Republic over six thousand years ago. Kind-hearted, the Kel Dor nevertheless \
+believe in swift justice. \" "
+
+personality = "PERSONALITY : \n \"Even-tempered and kind-hearted, the Kel Dor never turn away a bein in need. At the same time, they average Kel Dor believes in \
+quick, simple justice and isn't averse to taking the law into his or her own hands.\" "
+
+physicality = "PHYSICAL DESCRIPTION : \n \"The average Kel Dor stands between 1.4 and 2 meters tall. Their skin ranges in color from peach to deep red. Most have black \
+eyes, though few are born with silver irises that some believe mark them as strong in the Force.\""
+
+homeworld = "HOMEWORLD : \n \"The technological planet of Dorin.\""
+
+language = "LANGUAGE(S) : \n \"Speak Kel Dor and Basic\""
+
+example_names = "EXAMPLE NAMES : \n \"Dorn Tlo, Plo Koon, Torin Dol.\"" 
+
+adventuring = "WHY THEY VENTURE OUT: \n \"Kel Dor who decide to brave the dangers of liivng outside their homeworld tend to be nobles such as diplomats and traders, bounty \
+hunters and Jedi. Their willingness to help others often mixes with their innate sense of justice, leading many into some form of law enforcement.\""
+
+adjustments = "Species Abilities: \n \"+2 DEX | +2 WIS | -2 CON\""
+
+special = "\"Special Trait: Gas Breather: Requires their native atmosphere to survive. Without their goggles, Kel Dor are considered blind. Without the mask they will suffocate.\n \
+Special Trait: Low-Light Vision: Kel Dor can see twice as far as a Human in dim light (moonlight) when not in their native atmosphere.\"\n"
+
+strAdj = 0
+dexAdj = 2
+intAdj = 0
+wisAdj = 2
+conAdj = -2
+chaAdj = 0 
+
+trait_size = "Medium"
+trait_size.lower()
+size_bonus_defense = 0
+size_bonus_attack =  0
+size_bonus_hide =  0
+
+base_speed_meters = 10
+
+skill_pts = 4
+feats =  1
+
+auto_language = "Read/Write Basic , Speak Basic. "
+
+
