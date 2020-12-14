11697b9b7db79090d827d057e11d7fa6f3791634
ianmcneill7 <64739630+ianmcneill7@users.noreply.github.com>
2020-12-13 22:40:53 -0500

    Add files via upload

---
+++ SPECIESFILES/quarren.py
@@ -0,0 +1,51 @@
+# Quarren descriptions, traits and bonuses
+
+description = "DESCRIPTION : \n \"The humanoid Quarren share the watery world of Mon Calamari with the Mon Calamari. \
+While the Mon Calamari live on the surface, the Quarren dwell in air-filled underwater cities. Where the Mon Calamari \
+are curious and optimistic, the Quarren tend to be staid and pessimistic. They consider the Mon Calamari idealism to be \
+the result of foolish, even dangerous, dreaming that only leads to trouble. Many Quarren tend to be pragmatic, unwilling \
+to trust or accept new ideas.\" "
+
+personality = "PERSONALITY : \n \"Practical and conservative, Quarren tend to hate change and distrust anyone who displays \
+overt optimism and idealism.\" "
+
+physicality = "PHYSICAL DESCRIPTION : \n \"The average Quarren stands between 1.4 and 1.9 meters tall. They have heads that \
+resemble four-tentacled squids, hence the name \'squid head\' that has followed them to other worlds. They have leathery skin \
+and suction-cupped fingers. "
+
+homeworld = "HOMEWORLD : \n \"The watery, technological planet Mon Calamari.\""
+
+language = "LANGUAGE(S) : \n \"Quarren speak Quarrenese and Basic. They tend to learn Mon Calamarian.\""
+
+example_names = "EXAMPLE NAMES : \n \"Kelmut, Seggor, Tessek, Tsillin, Vekker, Vuhlg.\"" 
+
+adventuring = "WHY THEY VENTURE OUT: \n \"Quarren leave their homeworld to escape their dependency on the Mon Calamari. They \
+tend to seek out the fringes of society, operating as scoundrels or nobles in criminal organizations or with mercenary groups \
+pirates, outlaw bands, or smugglers.\""
+
+adjustments = "Species Abilities: \n \"+2 CON | -2 WIS | -2 CHA\""
+
+special = "\"Special Trait: Breathe Underwater: Cannot drown in water. \
+Special Trait: +4 species bonus on Swim checks. \"\n"
+
+strAdj = 0
+dexAdj = 0
+intAdj = 0
+wisAdj = -2
+conAdj = 2
+chaAdj = -2 
+
+trait_size = "Medium"
+trait_size.lower()
+size_bonus_defense = 0
+size_bonus_attack =  0
+size_bonus_hide =  0
+
+base_speed_meters = 10
+
+skill_pts = 4
+feats =  1
+
+auto_language = "Read/Write Basic , Speak Basic. "
+
+
