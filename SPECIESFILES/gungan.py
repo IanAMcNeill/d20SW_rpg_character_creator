e1867c4fdb5f7795b27d770c41fadbfa36a61e86
ianmcneill7 <64739630+ianmcneill7@users.noreply.github.com>
2020-12-13 21:17:52 -0500

    Update gungan.py

--- SPECIESFILES/gungan.py
+++ SPECIESFILES/gungan.py
@@ -1,10 +1,10 @@
-# Cerean descriptions, traits and bonuses
+# Gungan descriptions, traits and bonuses
 
 description = "DESCRIPTION : \n \"Gugnans are an ancient species from Naboo with a warlike tradition. Gungans can \
-    hold their breath for extended periods of time, and they live within underwater cities. The most common Gungan \
-    race is the Otolla Gungan, characterized by taller, leaner frames and extended eyestalks. The rare Ankura \
-    Gungan has recessed eyes, gren skin, and a heavier, shorter frame. They are usually distrustful of \
-    strangers and slow to accept aid, Gungans are nevertheless brave, strong, and confident of their own traditions.\" "
+hold their breath for extended periods of time, and they live within underwater cities. The most common Gungan \
+race is the Otolla Gungan, characterized by taller, leaner frames and extended eyestalks. The rare Ankura \
+Gungan has recessed eyes, gren skin, and a heavier, shorter frame. They are usually distrustful of \
+strangers and slow to accept aid, Gungans are nevertheless brave, strong, and confident of their own traditions.\" "
 
 personality = "PERSONALITY : \n \"Gungans are inquisive, cautious, and suspicious.\" "
 
@@ -17,17 +17,17 @@ language = "LANGUAGE(S) : \n \"Gungans speak gungan and Basic\""
 example_names = "EXAMPLE NAMES : \n \"Fassa, Jar Jar, Rugor, Tarpals, Toba, Tobler Ceel, Yoss.\"" 
 
 adventuring = "WHY THEY VENTURE OUT: \n \"Many leave their cities to explore the ancient swamps andshorelines. \
-    Since the treaty with the Humans of Naboo, some Gungans have taken a larger step into galacitc society.\
-     These brave individuals are driven by an impulse to explore and regain the warrior spirit of old. There \
-    have yet to be any Jedi from these people but there have been legends of adepts.\""
+Since the treaty with the Humans of Naboo, some Gungans have taken a larger step into galacitc society.\
+These brave individuals are driven by an impulse to explore and regain the warrior spirit of old. There \
+have yet to be any Jedi from these people but there have been legends of adepts.\""
 
 adjustments = "Species Abilities: \n \"-2 WIS | +2 CON (Preconfigured) \""
 
-special1 = "\"Special Trait: Gungans can hold their breath for a number of rounds equal to 25 times his CON score. \n \
-            Special Trait: +4 species bonus on Swim checks.\n \
-            Special Trait: Low-Light vision: Can see twice as far as humans in dim light.\n \
-            Special Trait: +2 species bonus on Listen checks \n \
-            Special Trait: +1 species bonus on Reflex saves\"\n"
+special = "\"Special Trait: Gungans can hold their breath for a number of rounds equal to 25 times his CON score. \n \
+Special Trait: +4 species bonus on Swim checks.\n \
+Special Trait: Low-Light vision: Can see twice as far as humans in dim light.\n \
+Special Trait: +2 species bonus on Listen checks \n \
+Special Trait: +1 species bonus on Reflex saves\"\n"
 
 
 strAdj = 0
