2be524a322e4fa3d326b5f6f973b99a4a0e6809d
ianmcneill7 <64739630+ianmcneill7@users.noreply.github.com>
2020-12-13 21:07:15 -0500

    Update cerean.py

--- SPECIESFILES/cerean.py
+++ SPECIESFILES/cerean.py
@@ -1,14 +1,14 @@
 # Cerean descriptions, traits and bonuses
 
 description = "DESCRIPTION : \n \"Cereans come from a remote Utopian world. Most are peaceful, following farming traditions that date back thousands \
-    of years. The species includes more females than males, and their society is matriarchal. Their high-domed heads hold binary brains, and two hearts\
-    beat in a Cerean's chest. With a non-aggressive heritage, it is only rarely that a Cerean develops the desire to adventure, and even more rare\
-     that one takes up the robes of a Jedi Knight.\" "
+of years. The species includes more females than males, and their society is matriarchal. Their high-domed heads hold binary brains, and two hearts\
+beat in a Cerean's chest. With a non-aggressive heritage, it is only rarely that a Cerean develops the desire to adventure, and even more rare\
+that one takes up the robes of a Jedi Knight.\" "
 
 personality = "PERSONALITY : \n \"Tend to be calm, rational, and extremely logical\" "
 
 physicality = "PHYSICAL DESCRIPTION : \n \"Average about 2 meters in height, with elogated craniums. They have a maturity rate and life span similar\
-     to that of Humans.\""
+to that of Humans.\""
 
 homeworld = "HOMEWORLD : \n \"Cerea\""
 
@@ -17,12 +17,12 @@ language = "LANGUAGE(S) : \n \"Speak and read Cerean and Basic.\""
 example_names = "EXAMPLE NAMES : \n \"Ki-Adi, So Leet, Sylvn, Ti-Dal, Maj-Odo.\"" 
 
 adventuring = "WHY THEY VENTURE OUT: \n \"Would be adventurers do so in direct contradiction to their traditions and beliefs. Aggression is \
-    a last resort only after all peaceful solutions are attempted. Their binary brains allow them to give equal weight to two different sides \
-    of an argument. A Force-user will often contemplate the light side and dark side simultaneously.\""
+a last resort only after all peaceful solutions are attempted. Their binary brains allow them to give equal weight to two different sides \
+of an argument. A Force-user will often contemplate the light side and dark side simultaneously.\""
 
 adjustments = "Species Abilities: \n \" +2 Int | +2 Wis | -2 Dex (Preconfigured) \""
 
-special1 = "\"Special Trait: +2 species bonus on Initiatve checks.\"\n"
+special = "\"Special Trait: +2 species bonus on Initiatve checks.\"\n"
 
 strAdj = 0
 dexAdj = -2
