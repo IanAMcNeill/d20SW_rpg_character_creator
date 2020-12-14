48f3cc55ad4bf535050cbec38821458243ca4493
ianmcneill7 <64739630+ianmcneill7@users.noreply.github.com>
2020-12-13 22:54:47 -0500

    Add files via upload

---
+++ SPECIESFILES/rodian.py
@@ -0,0 +1,50 @@
+# Rodian descriptions, traits and bonuses
+
+description = "DESCRIPTION : \n \"Rodians grow up in a culture base on hunting and tracking. They consider bounty hunting to be an art form. \
+They romanticize death and the hunt, revering gladitorial games and violence. They come from a tightly controlled society on an industrial world \
+where the greatest aspiration is to become the best hunter in the galaxy.\" "
+
+personality = "PERSONALITY : \n \"Rodians tend to be violent, tenacious, and dedicated.\" "
+
+physicality = "PHYSICAL DESCRIPTION : \n \"Humanoid, with multifaceted eyes, a tapered snout, and deep green skin, the average Rodian stands between \
+1.5 and 1.7 meters tall.\""
+
+homeworld = "HOMEWORLD : \n \"Rodians come from the industrial world Rodia.\""
+
+language = "LANGUAGE(S) : \n \"Rodians speak Rodese and Basic, but many learn to speak Huttese as well.\""
+
+example_names = "EXAMPLE NAMES : \n \"Andoorni, Beedo, Chido, Doda, Greedo, Greeata, Kelko, Navik, Neela, Neesh, Wald.\"" 
+
+adventuring = "WHY THEY VENTURE OUT: \n \"Rodian adventurers leave their homeworld to improve their skills, hoping to one day return and claim \
+the title of Hunt Master. To this end, they take on roles that allow them to flex their hunting skills, increase their battle prowess, \
+and ultimately gain experience to someday earn the prestigious profession of \'bounty hunter.\' Few Rodians have anything to do with the Force, \
+so Rodian Force-users are extremely rare.\""
+
+adjustments = "Species Abilities: \n \"+2 DEX | -2 WIS | -2 CHA\""
+
+special = "\"Special Trait: Rodians receive the Track feat as a bonus feat at 1st level. \n\
+Special Trait: +2 species bonus on Listen checks. \n\
+Special Trait: +2 species bonus on Search checks. \n\
+Special Trait: +2 species bonus on Spot checks.\"\n"
+
+strAdj = 0
+dexAdj = 2
+intAdj = 0
+wisAdj = -2
+conAdj = 0
+chaAdj = -2 
+
+trait_size = "Medium"
+trait_size.lower()
+size_bonus_defense = 0
+size_bonus_attack =  0
+size_bonus_hide =  0
+
+base_speed_meters = 10
+
+skill_pts = 4
+feats =  1
+
+auto_language = "Speak and Read/Write Rodese and Basic."
+
+
