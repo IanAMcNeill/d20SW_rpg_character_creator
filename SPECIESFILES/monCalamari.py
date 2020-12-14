bd1b19d8325c08bd0a0eeb5a53e832805a39f88d
ianmcneill7 <64739630+ianmcneill7@users.noreply.github.com>
2020-12-13 22:25:45 -0500

    Add files via upload

---
+++ SPECIESFILES/monCalamari.py
@@ -0,0 +1,51 @@
+# Mon Calamari descriptions, traits and bonuses
+
+description = "DESCRIPTION : \n \"Soft-spoken but forceful, the mon Calamari are great starship engineers. They see everything they create as a work of art, not just as a tool \
+or weapon. They are a lawful, organized people who have little tolerance for those who operate outside the status quo. They come from a water-covered world and can breathe both \
+air and water. Recognized for their analytical and organizational skills, Mon Calamari also have a keen love of flying among the stars. They believe in order and the greater good, \
+sometimes going so far as to support the good of society over the good of the individual.\" "
+
+personality = "PERSONALITY : \n \"Creative, quiet, and inquisitive, the Mon Calamari are dreamers who cherish peace but aruen't afraid to wage war for the causes they believe in.\" "
+
+physicality = "PHYSICAL DESCRIPTION : \n \"The average Mon Calamari stands between 1.3 and 1.8 meters tall. They smell of salt and the sea with high-domed heads, large eyes, and smooth \
+mottled skin.\""
+
+homeworld = "HOMEWORLD : \n \"The watery, technological planet of Mon Calamari.\""
+
+language = "LANGUAGE(S) : \n \"Speak Mon Calamari and Basic and tend to learn Quarrenese as well.\""
+
+example_names = "EXAMPLE NAMES : \n \"Ackbar, Bant, Cilghal, Ibtisam, Jesmin, Oro, Perit, Rekara.\"" 
+
+adventuring = "WHY THEY VENTURE OUT: \n \"Mon Calamari seek their dreams among the stars. They are idealistic and daring, often attaching themselves to causes that seem hopeless or lost \
+right from the start. An individual Mon Calamari has great spirit and an enthusiasm that often remains hidden behind a quiet, orderly exterior. They strive to prove that even thinkers and \
+dreamers can be brave and daring when the need arises.\""
+
+adjustments = "Species Abilities: \n \"+2 INT | -2 CON (Preconfigured) \""
+
+special = "Special Trait: Breathe Underwater: +4 species bonus on Swim checks and cannot drown in water. \n \
+Special Trait: Low-Light Vision: Can see twice as far as humans in dim light. \n \
+Special Trait: +4 species bonus on Craft checks (select on Craft skill for each individual), due to their artistic nature. \n \
+Special Trait: +1 species bonus on Will saving throws when in moist or watery environments: extremely uncomfortable in dry environments. \n \
+Special Trait: -1 species penalty on Will saving throws when in dry or arid environments. \n"
+
+strAdj = 0
+dexAdj = 0
+intAdj = 2
+wisAdj = 0
+conAdj = -2
+chaAdj = 0 
+
+trait_size = "Medium"
+trait_size.lower()
+size_bonus_defense = 0
+size_bonus_attack =  0
+size_bonus_hide =  0
+
+base_speed_meters = 10
+
+skill_pts = 4
+feats =  1
+
+auto_language = "Read/Write Basic , Speak Basic. "
+
+
