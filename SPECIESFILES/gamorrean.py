fc269a24933c4bf8e1d15b138df569ef6c627958
ianmcneill7 <64739630+ianmcneill7@users.noreply.github.com>
2020-12-13 21:01:06 -0500

    Update gamorrean.py

--- SPECIESFILES/gamorrean.py
+++ SPECIESFILES/gamorrean.py
@@ -1,25 +1,42 @@
 # Gamorean descriptions, traits and bonuses
 
-description = "DESCRIPTION : \n \"Brutish and violent, the porcine Gamorreans come from the Outer Rim world of Gamorr. Green-skinned with piglike snouts, tusks, and small horns, Gamorreans are respected for their great strength and short tempers. The history of Gamorr is marked by an almost constant state of warfare. Males train for and fight wars, while the females farm, hunt, and handle trade. Since their discovery, many have found their way into galactic society. They come from a poorly technological society and their primitive culture resembles the preindustrial periods that human civilizations experienced more than 25,000 years ago. They make great laborers, bodyguards, and mercenaries. While they prefer primitive melee weapons, they have no problem learning to use powered weapons and even blasters.\" "
+description = "DESCRIPTION : \n \"Brutish and violent, the porcine Gamorreans come from the Outer Rim world of Gamorr. \
+    Green-skinned with piglike snouts, tusks, and small horns, Gamorreans are respected for their great strength and short\
+    tempers. The history of Gamorr is marked by an almost constant state of warfare. Males train for and fight wars, \
+    while the females farm, hunt, and handle trade. Since their discovery, many have found their way into galactic\
+     society. They come from a poorly technological society and their primitive culture resembles the preindustrial\
+    periods that human civilizations experienced more than 25,000 years ago. They make great laborers, bodyguards,\
+    and mercenaries. While they prefer primitive melee weapons, they have no problem learning to use powered weapons and even blasters.\" "
 
-personality = "PERSONALITY : \n \"A brutish, violent, and proud species, Gamorreans respect physical prowess and have no problem facing death against a foe they feel they are on at least equal footing with. They do display fear in the face of obviously superior opponents, however, considering such a display to be prudent and even rational.\" "
+personality = "PERSONALITY : \n \"A brutish, violent, and proud species, Gamorreans respect physical prowess and have \
+    no problem facing death against a foe they feel they are on at least equal footing with. They do display fear in \
+    the face of obviously superior opponents, however, considering such a display to be prudent and even rational.\" "
 
-physicality = "PHYSICAL DESCRIPTION : \n \"At an average heigh of 1.8m tall, this species has thick green skin covering their powerful muscles. Close-set eyes, a thick snout, tusks, and small horns lend a distant look to every Gamorrean.\""
+physicality = "PHYSICAL DESCRIPTION : \n \"At an average heigh of 1.8m tall, this species has thick green skin covering \
+    their powerful muscles. Close-set eyes, a thick snout, tusks, and small horns lend a distant look to every Gamorrean.\""
 
 homeworld = "HOMEWORLD : \n \"The preindustrial agricultural world of Gamorr.\""
 
-language = "LANGUAGE(S) : \n \"Gammoreans speak Gamorrean. They have no written version of the language. they can learn to understand other languages, but Gamorreans lack the mechanism to speak any tongue but their own.\""
+language = "LANGUAGE(S) : \n \"Gammoreans speak Gamorrean. They have no written version of the language. they can learn to\
+     understand other languages, but Gamorreans lack the mechanism to speak any tongue but their own.\""
 
 example_names = "EXAMPLE NAMES : \n \"Gartogg, Jubnuk, Ortugg, Ugmush, Venorra, Warlug.\"" 
 
-adventuring = "WHY THEY VENTURE OUT: \n \"They leave their homeworld for many reasons. Some are taken as slaves and later escape or buy out their contracts. Others sell their contracts on the open market, finding employment as bodyguards, mercenaries, and professional soldiers. A Gamorrean is constantly at war, no matter where he or she resides in the galaxy: it's often only a matter of figuring out who the enemy is. Gamorrean heroes tend to select the fringer or soldier classes. To date, there are no known examples of Gamorrean Force-users.\""
+adventuring = "WHY THEY VENTURE OUT: \n \"They leave their homeworld for many reasons. Some are taken as slaves and later \
+    escape or buy out their contracts. Others sell their contracts on the open market, finding employment as bodyguards,\
+     mercenaries, and professional soldiers. A Gamorrean is constantly at war, no matter where he or she resides in the\
+    galaxy: it's often only a matter of figuring out who the enemy is. Gamorrean heroes tend to select the fringer or \
+    soldier classes. To date, there are no known examples of Gamorrean Force-users.\""
 
 adjustments = "Species Abilities: \n \"+2 Str | -2 Dex | -2 Int (Preconfigured) \""
 
-pecial1 = "\"Special Trait: Primitive: Receives the bonus Weapon Group Prociency (primitive weapons)  at 1st level instead of usual Weapon Group Proficiency feats available to whatever class the character selects.  \n \
-            Special Trait: Primitive: Receives Weapon Group Proficiency (simple weapons)  at 1st level instead of usual Weapon Group Proficiency feats available to whatever class the character selects.\
-            Special Trait: +2 species bonus on Fortitude saves. \
+pecial1 = "\"Special Trait: Primitive: Receives the bonus Weapon Group Prociency (primitive weapons)  at 1st level \
+    instead of usual Weapon Group Proficiency feats available to whatever class the character selects.  \n \
+            Special Trait: Primitive: Receives Weapon Group Proficiency (simple weapons)  at 1st level instead \
+                of usual Weapon Group Proficiency feats available to whatever class the character selects.\n \
+            Special Trait: +2 species bonus on Fortitude saves. \n \
             Special Trait: Bonus feat Power Attack at 1st Level.  \"\n"
+
 strAdj = +2
 dexAdj = -2
 intAdj = -2
