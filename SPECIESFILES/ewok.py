729e91ef6bfbfc7a9c3183f02b07d8d9fe52c0cb
ianmcneill7 <64739630+ianmcneill7@users.noreply.github.com>
2020-12-13 21:12:00 -0500

    Update ewok.py

--- SPECIESFILES/ewok.py
+++ SPECIESFILES/ewok.py
@@ -1,14 +1,14 @@
 # Ewok descriptions, traits and bonuses
 
 description = "DESCRIPTION : \n \"Ewoks hail from the forest moon of Endor, where they maintain a primitive hunter/gatherer\
-     society. Prior to the Battle of Endor, Ewoks were almost entirely unknown to the galaxy at larger. Some tradrs and explorers \
-         visited Endor's moon, but with no form of space travel, Ewoks never left their homeworld. Although primitive by galactic \
-             standards, the tribal Ewoks are resourceful and highly adaptable.\" "
+society. Prior to the Battle of Endor, Ewoks were almost entirely unknown to the galaxy at larger. Some tradrs and explorers \
+visited Endor's moon, but with no form of space travel, Ewoks never left their homeworld. Although primitive by galactic \
+standards, the tribal Ewoks are resourceful and highly adaptable.\" "
 
 personality = "PERSONALITY : \n \"Ewoks tend to be curious, superstitious, and courageous, though they can be fearful around things that are strange and new.\" "
 
 physicality = "PHYSICAL DESCRIPTION : \n \"Ewoks average about 1m tall with thick fur that varies in color and patter. They often \
-     wear hoods, decorative feathers, and animal bones.\""
+wear hoods, decorative feathers, and animal bones.\""
 
 homeworld = "HOMEWORLD : \n \"The forest moon of Endor\""
 
@@ -17,19 +17,19 @@ language = "LANGUAGE(S) : \n \"Ewoks speak Ewokese. They have no written form of
 example_names = "EXAMPLE NAMES : \n \"Asha, Chirpa, Deej, Kneesaa, Latara, Logray, Malani, Nippet, Paploo, Shodu, Teebo, Wicket, Wiley.\"" 
 
 adventuring = "WHY THEY VENTURE OUT: \n \"May be motivated by a love of excitement, a natural inquisitiveness, or a warrior's quest. \
-    Usually, an Ewok hero joins u with a scout or other hero who has accccess to a starship. Collecting powerful \"magic\" items from \
-    fallen opponents, such as a stormtrooper's helmet or a range finder from a blaster rifle, is a sure way for an Ewok to earn the respect of his tribe.\""
+Usually, an Ewok hero joins u with a scout or other hero who has accccess to a starship. Collecting powerful \"magic\" items from \
+fallen opponents, such as a stormtrooper's helmet or a range finder from a blaster rifle, is a sure way for an Ewok to earn the respect of his tribe.\""
 
 adjustments = "Species Abilities: \n \"+2 Dex | -2 Str (Preconfigured) \""
 
-special1 = "\"Special Trait: +1 size bonus to Defense. \n \
-            Special Trait: +4 size bonus on Hide checks (must use smaller weapons than humans use).\n \
-            Special Trait: Lifting and carrying capacity are three-quarters of those of Medium-size characters. \n \
-            Special Trait: Weapon Group Proficiency (primitive weapons) at 1st level instead of usual Weapon Group Proficiency \
-                feats available to whatever class the character selects. \n \
-            Special Trait: Weapon Group Proficiency (simple weapons) at 1st level instead of usual Weapon Group Proficiency \
-                feats available to whatever class the character selects. \
-            Special Trait: Receives the Alertness feat as a bonus feat at 1st level. \"\n"
+special = "\"Special Trait: +1 size bonus to Defense. \n \
+Special Trait: +4 size bonus on Hide checks (must use smaller weapons than humans use).\n \
+Special Trait: Lifting and carrying capacity are three-quarters of those of Medium-size characters. \n \
+Special Trait: Weapon Group Proficiency (primitive weapons) at 1st level instead of usual Weapon Group Proficiency \
+feats available to whatever class the character selects. \n \
+Special Trait: Weapon Group Proficiency (simple weapons) at 1st level instead of usual Weapon Group Proficiency \
+feats available to whatever class the character selects. \n \
+Special Trait: Receives the Alertness feat as a bonus feat at 1st level. \"\n"
 
 strAdj = -2
 dexAdj = 2
